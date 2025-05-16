6891581f971f19d877388598b7569e3a12cf45f5
Axel Schneewind <axel.schneewind@protonmail.com>
2025-05-16 16:47:49 +0800

    add functions to set invalid/valid data

--- setdata.h
+++ setdata.h
@@ -1,3 +1,11 @@
 void setdata(char* data, int ctr) {
     data[0] = ((ctr % 2) * (ctr % 3) * (ctr % 7)) ? 'a' : 0;
 }
+
+void setdata_valid(char* data) {
+    data[0] = 'a';
+}
+
+void setdata_invalid(char* data) {
+    data[0] = 'a';
+}
