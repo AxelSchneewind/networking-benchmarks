28bfba112afd7fe83bf91197f5fdf718743b9c1c
Axel Schneewind <axel.schneewind@protonmail.com>
2025-05-16 14:37:10 +0800

    make xdp-filter use same filter as userspace

--- xdp-filter/xdp_filter_kern.c
+++ xdp-filter/xdp_filter_kern.c
@@ -13,20 +13,20 @@
 
 #include <netinet/udp.h>
 
+#include "filter.h"
+
 SEC("xdp")
 int  xdp_prog_simple(struct xdp_md *ctx)
 {
 	int verdict = XDP_DROP;
 
+    // access payload
 	uint8_t *udp_bytes = (uint8_t*)(ctx->data + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct udphdr));
-
-	// check first character of data
-	if ((udp_bytes + 1) < (uint8_t*)(long)ctx->data_end) {
-		// accept
-		if (*udp_bytes == '0') {
-			verdict = XDP_PASS;
-		}
-	}
+    if ((udp_bytes + 1) < (uint8_t*)(long)ctx->data_end) {
+        if (filter(udp_bytes)) {
+            verdict = XDP_PASS;
+        }
+    }
 
 	return verdict;
 }
